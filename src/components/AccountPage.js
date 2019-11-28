d5d29a1a245b87002a8e07a5c402a37ab3ffb25b
lottenoorlander <lottenoorlander@gmail.com>
2019-11-28 16:33:59 +0100

    account page only shows if logged in

--- src/components/AccountPage.js
+++ src/components/AccountPage.js
@@ -3,15 +3,26 @@ import { deleteAccount } from "../store/auth/actions";
 import { connect } from "react-redux";
 
 class AccountPage extends Component {
+  state = {
+    account: true
+  };
+
   clickHandler = () => {
     this.props.dispatch(deleteAccount);
+    this.setState({ account: false });
   };
 
   render() {
     return (
       <div>
-        <h1>Account Page</h1>
-        <button onClick={this.clickHandler}>Delete your account</button>
+        {this.state.account ? (
+          <div>
+            <h1>Account Page</h1>
+            <button onClick={this.clickHandler}>Delete your account</button>
+          </div>
+        ) : (
+          <p>You need to be logged in to see this page</p>
+        )}
       </div>
     );
   }
