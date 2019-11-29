5a1d65af4fa7a6e9d077d9bffd7b50cd07357e8c
lottenoorlander <lottenoorlander@gmail.com>
2019-11-29 14:06:21 +0100

    minor fixes

--- src/store/developers/reducer.js
+++ src/store/developers/reducer.js
@@ -1,4 +1,4 @@
-const initialState = null;
+const initialState = { developers: null, currentDeveloper: {} };
 
 export default function reducer(state = initialState, action) {
   switch (action.type) {
